d7b71110049163c883cab22b57c222680732becd
pramod-practo <pramod.m@practo.com>
2018-03-20 20:33:06 +0530

    Create the project

---
+++ LocalizationMedium/LocalizationMedium/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocalizationMedium
+//
+//  Created by Pramod M on 20/03/18.
+//  Copyright © 2018 Pramod M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
